e1c78e141107831e2e02f38c13c56d8b750ecf77
Victor Fernando Conti <victorconti@finchsolucoes.com.br>
2020-11-25 00:25:23 -0300

    Initial release

---
+++ hd-service/src/main/java/br/com/victor/streammermultiquality/hdservice/HdServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.victor.streammermultiquality.hdservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdServiceApplication.class, args);
+	}
+
+}
