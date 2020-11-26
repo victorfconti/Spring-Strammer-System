dfcafaa3c65853bbf32736692798779b455004a0
Victor Fernando Conti <victorconti@finchsolucoes.com.br>
2020-11-25 22:47:12 -0300

    Criação de um serviço pocket

---
+++ pocket/src/main/java/br/com/victor/streammermultiquality/hdservice/PocketServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.victor.streammermultiquality.hdservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocketServiceApplication.class, args);
+	}
+
+}
