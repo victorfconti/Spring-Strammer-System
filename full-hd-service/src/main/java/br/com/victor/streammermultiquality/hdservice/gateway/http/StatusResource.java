4a0e680a883d82e6ab340a440691561a594cd1b0
Victor Fernando Conti <victorconti@finchsolucoes.com.br>
2020-11-30 22:59:36 -0300

    Creation of heath check for gateway

--- full-hd-service/src/main/java/br/com/victor/streammermultiquality/hdservice/gateway/http/StatusResource.java
+++ full-hd-service/src/main/java/br/com/victor/streammermultiquality/hdservice/gateway/http/StatusResource.java
@@ -14,4 +14,10 @@ public class StatusResource {
         return "OK";
     }
 
+    @GetMapping("/health")
+    @ResponseStatus(HttpStatus.OK)
+    public String health(){
+        return "OK";
+    }
+
 }
